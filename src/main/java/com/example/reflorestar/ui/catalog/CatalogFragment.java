41a9240ff36a3325657cdf6f313280881fde28fe
Enrico Florentino <2181895@my.ipleiria.pt>
2021-04-20 00:44:48 +0100

    Catalog search input basic functionality and generic result list

--- src/main/java/com/example/reflorestar/ui/catalog/CatalogFragment.java
+++ src/main/java/com/example/reflorestar/ui/catalog/CatalogFragment.java
@@ -1,22 +1,36 @@
 package com.example.reflorestar.ui.catalog;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.reflorestar.R;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class CatalogFragment extends Fragment {
 
     private CatalogViewModel catalogViewModel;
+    private TextInputLayout searchText;
+    private Button searchButton;
+    private ListView listView;
+    private CustomAdapter adapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,8 +38,125 @@ public class CatalogFragment extends Fragment {
                 new ViewModelProvider(this).get(CatalogViewModel.class);
         View root = inflater.inflate(R.layout.fragment_catalog, container, false);
 
+        searchText = root.findViewById(R.id.catalogSearch);
+        Editable editText = searchText.getEditText().getText();
+
+        searchButton = root.findViewById(R.id.searchButton);
+        searchButton.setOnClickListener(view -> {
+            if(!editText.toString().matches("")){
+                Toast.makeText(root.getContext(), editText, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        listView = (ListView) root.findViewById(R.id.resultList);
+
+        final ArrayList<HashMap<String,Object>> list = new ArrayList<HashMap<String, Object>>();
+
+        HashMap<String,Object> map1 = new HashMap<String,Object>();
+        map1.put("Image",R.drawable.ic_launcher_background);
+        map1.put("FirstLastName", "FirstLastName 1");
+        map1.put("Descriptions","Descriptions 1 Descriptions 1 Descriptions 1 Descriptions 1 Descriptions 1 Descriptions 1 Descriptions 1 Descriptions 1");
+        map1.put("Params1","Params1 1");
+        map1.put("Params2","Params2 1");
+        map1.put("Params3","Params3 1");
+        list.add(map1);
+
+        HashMap<String,Object> map2 = new HashMap<String,Object>();
+        map2.put("Image",R.drawable.ic_launcher_foreground);
+        map2.put("FirstLastName","FirstLastName 2");
+        map2.put("Descriptions","Descriptions 2 Descriptions 2 Descriptions 2 Descriptions 2 Descriptions 2 Descriptions 2 Descriptions 2 Descriptions 2");
+        map2.put("Params1","Params1 2");
+        map2.put("Params2","Params2 2");
+        map2.put("Params3","Params3 2");
+        list.add(map2);
+
+        HashMap<String,Object> map3 = new HashMap<String,Object>();
+        map3.put("Image",R.drawable.ic_home_black_24dp);
+        map3.put("FirstLastName","FirstLastName 3");
+        map3.put("Descriptions","Descriptions 3 Descriptions 3 Descriptions 3 Descriptions 3 Descriptions 3 Descriptions 3 Descriptions 3 Descriptions 3");
+        map3.put("Params1","Params1 3");
+        map3.put("Params2","Params2 3");
+        map3.put("Params3","Params3 3");
+        list.add(map3);
+
+        adapter = new CustomAdapter(root.getContext(),list);
+        listView.setAdapter(adapter);
+        listView.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(root.getContext(),list.get(position).get("FirstLastName").toString(),Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
 
         return root;
     }
+
+    class CustomAdapter extends BaseAdapter {
+        private Context context;
+        private ArrayList<HashMap<String,Object>> data;
+
+        public CustomAdapter(Context context,ArrayList<HashMap<String,Object>> data){
+            this.context = context;
+            this.data = data;
+        }
+
+        @Override
+        public int getCount(){
+            return data.size();
+        }
+
+        @Override
+        public Object getItem(int position){
+            return data.get(position);
+        }
+
+        @Override
+        public long getItemId(int position){
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent){
+            ViewHolder holder;
+            if(convertView == null){
+                holder =new ViewHolder();
+                convertView = LayoutInflater.from(context).inflate(R.layout.list_item, null);
+                holder.imgIcon = convertView.findViewById(R.id.imgIcon);
+                holder.txtFirstLastName = convertView.findViewById(R.id.txtFirstLastName);
+                holder.txtDescription = convertView.findViewById(R.id.txtDescription);
+                holder.txtParams1 = convertView.findViewById(R.id.txtParams1);
+                holder.txtParams2 = convertView.findViewById(R.id.txtParams2);
+                holder.txtParams3 = convertView.findViewById(R.id.txtParams3);
+                convertView.setTag(holder);
+            }else{
+                holder = (ViewHolder)convertView.getTag();
+            }
+
+            holder.imgIcon.setImageResource((Integer) data.get(position).get("Image"));
+            holder.txtFirstLastName.setText(data.get(position).get("FirstLastName").toString());
+            holder.txtDescription.setText(data.get(position).get("Descriptions").toString());
+            holder.txtParams1.setText(data.get(position).get("Params1").toString());
+            holder.txtParams2.setText(data.get(position).get("Params2").toString());
+            holder.txtParams3.setText(data.get(position).get("Params3").toString());
+
+
+            return convertView;
+        }
+
+        class ViewHolder{
+            ImageView imgIcon;
+            TextView txtFirstLastName;
+            TextView txtDescription;
+            TextView txtParams1;
+            TextView txtParams2;
+            TextView txtParams3;
+        }
+    }
+
 }
