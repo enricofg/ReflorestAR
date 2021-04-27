c94a292381752e3495ab734904070fac4e8edb41
Enrico Florentino <2181895@my.ipleiria.pt>
2021-04-27 02:12:13 +0100

    Camera interface base

--- src/main/java/com/example/reflorestar/CameraActivity.java
+++ src/main/java/com/example/reflorestar/CameraActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.MotionEvent;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.ar.core.Anchor;
@@ -19,6 +20,9 @@ public class CameraActivity extends AppCompatActivity {
 
     private ArFragment arFragment;
     private ModelRenderable modelRenderable;
+    private Button closeButton;
+    private Button pineTreeButton;
+    private Button elmTreeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,13 +34,39 @@ public class CameraActivity extends AppCompatActivity {
             getSupportActionBar().hide();
         }
 
+        closeButton = findViewById(R.id.closeButton);
+        pineTreeButton = findViewById(R.id.buttonPineTree);
+        elmTreeButton = findViewById(R.id.buttonElmTree);
+
+        closeButton.setOnClickListener(view -> {
+                finish();
+        });
+
+        pineTreeButton.setOnClickListener(view -> {
+            setUpPineTreeModel();
+        });
+
+        elmTreeButton.setOnClickListener(view -> {
+            setUpElmTreeModel();
+        });
+
         arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.fragment);
-        setUpModel();
+        setUpElmTreeModel();
         setUpPlane();
     }
 
-    private void setUpModel() {
+    private void setUpElmTreeModel() {
         ModelRenderable.builder().setSource(this, R.raw.elmtree)
+                .build()
+                .thenAccept(renderable -> modelRenderable = renderable)
+                .exceptionally(throwable -> {
+                    Toast.makeText(CameraActivity.this, "Model can't be loaded", Toast.LENGTH_SHORT).show();
+                    return null;
+                });
+    }
+
+    private void setUpPineTreeModel() {
+        ModelRenderable.builder().setSource(this, R.raw.pinetree)
                 .build()
                 .thenAccept(renderable -> modelRenderable = renderable)
                 .exceptionally(throwable -> {
